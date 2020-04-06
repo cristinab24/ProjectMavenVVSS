8e7840b8f200567ef5f21f234a8709365a73386e
Cristina Braga <cristina.braga-extern@renault.com>
2020-04-06 11:08:38 +0300

    more changes

--- src/main/java/org/vvss/project/Validator/TemeValidator.java
+++ src/main/java/org/vvss/project/Validator/TemeValidator.java
@@ -7,14 +7,14 @@ public class TemeValidator implements Validator<Teme> {
     public String validate(Teme t) {
         String m;
         m = "";
+        if(t.getDescriere()==null || t.getDescriere().equals(""))
+            m = m + "\nDescriere invalida";
         if (t.getID() == null || t.getID().equals("") || t.getID() < 1)
             m = m + "\nID invalid";
         if (t.getDeadline() > 14 || t.getDeadline() < 1 || t.getDeadline() < t.getSapt_primire())
             m = m + "\nDeadline invalid";
         if (t.getSapt_primire() > 14 || t.getSapt_primire() < 1)
             m = m + "\nSaptamana in care tema a fost primita este invalida";
-        if(t.getDescriere()==null || t.getDescriere().equals(""))
-            m = m + "\nDescriere invalida";
         return m;
     }
 }
